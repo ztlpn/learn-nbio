2395844a6e77cd4fe9b5b68e1fe7fee8abf24c13
Alex Zatelepin <mvzp10@gmail.com>
2019-09-04 22:22:28 +0300

    [minitokio] access current runtime via thread-local var

--- src/bin/http_minitokio.rs
+++ src/bin/http_minitokio.rs
@@ -15,37 +15,118 @@ use slab::Slab;
 // that's the toplevel future
 type TaskBox = Pin<Box<dyn Future<Output = ()>>>;
 
-struct Runtime {
+struct RuntimeInner {
     poll: mio::Poll,
     io_resources: Slab<Vec<task::Waker>>,
-     // toplevel futures go here. the task is either in tasks or in ready_queue.
+    // toplevel futures go here. the task is either in tasks or in ready_queue.
     tasks: Slab<Option<TaskBox>>,
     ready_queue: VecDeque<(TaskBox, usize)>,
 }
 
-type RuntimePtr = Arc<RefCell<Runtime>>;
+impl RuntimeInner {
+    fn spawn<T>(&mut self, task: T) where T: Future<Output = ()> + 'static {
+        let pinned = Box::pin(task);
+        let key = self.tasks.insert(None);
+        self.ready_queue.push_back((pinned, key));
+    }
+}
+
+type RuntimeInnerHandle = Arc<RefCell<RuntimeInner>>;
+
+struct Runtime {
+    inner: RuntimeInnerHandle,
+}
+
+thread_local! {
+    static RUNTIME: RefCell<Option<RuntimeInnerHandle>> = RefCell::default();
+}
+
+fn current_runtime() -> RuntimeInnerHandle {
+    RUNTIME.with(|r| {
+        r.borrow().as_ref().expect("no current runtime!").clone()
+    })
+}
+
+struct RunGuard<'a> {
+    _runtime: &'a Runtime,
+}
+
+impl<'a> RunGuard<'a> {
+    fn new(runtime: &Runtime) -> RunGuard {
+        RUNTIME.with(|r| {
+            *r.borrow_mut() = Some(runtime.inner.clone());
+        });
+
+        RunGuard { _runtime: runtime }
+    }
+}
+
+impl<'a> Drop for RunGuard<'a> {
+    fn drop(&mut self) {
+        RUNTIME.with(|r| {
+            *r.borrow_mut() = None;
+        });
+    }
+}
+
+static WAKER_VTABLE: task::RawWakerVTable = {
+    unsafe fn clone_fn(task_idx: *const ()) -> task::RawWaker {
+        task::RawWaker::new(task_idx, &WAKER_VTABLE)
+    }
+
+    unsafe fn wake_fn(task_idx: *const ()) {
+        // consumes the Box
+        let task_idx: usize = std::mem::transmute(task_idx);
+        let runtime_handle = current_runtime();
+        let mut runtime = runtime_handle.borrow_mut();
+        let task = runtime.tasks.get_mut(task_idx).expect("can't wake nonexistent task");
+        if let Some(task) = task.take() {
+            runtime.ready_queue.push_back((task, task_idx))
+        }
+    }
+
+    unsafe fn wake_by_ref_fn(task_idx: *const ()) {
+        wake_fn(task_idx);
+    }
+
+    unsafe fn drop_fn(_task_idx: *const ()) {
+    }
+
+    task::RawWakerVTable::new(clone_fn, wake_fn, wake_by_ref_fn, drop_fn)
+};
+
+fn new_waker(task_idx: usize) -> task::Waker {
+    unsafe {
+        task::Waker::from_raw(task::RawWaker::new(std::mem::transmute(task_idx), &WAKER_VTABLE))
+    }
+}
+
+pub fn spawn<T>(task: T)
+where T: Future<Output = ()> + 'static {
+    current_runtime().borrow_mut().spawn(task);
+}
 
 impl Runtime {
-    fn new() -> io::Result<RuntimePtr> {
-        let res = Runtime {
+    pub fn new() -> io::Result<Runtime> {
+        let inner = RuntimeInner {
             poll: mio::Poll::new()?,
             io_resources: Slab::new(),
             tasks: Slab::new(),
             ready_queue: VecDeque::new(),
         };
 
-        Ok(Arc::new(RefCell::new(res)))
+        Ok(Runtime { inner: Arc::new(RefCell::new(inner)), })
     }
 
-    fn spawn<T>(&mut self, task: T)
+    pub fn run<T>(&mut self, task: T) -> io::Result<()>
     where T: Future<Output = ()> + 'static {
-        let pinned = Box::pin(task);
-        let key = self.tasks.insert(None);
-        self.ready_queue.push_back((pinned, key));
-    }
+        let _run_guard = RunGuard::new(self);
 
-    fn run(runtime: &RuntimePtr) -> io::Result<()> {
+        let runtime = &self.inner;
+        runtime.borrow_mut().spawn(task);
+
         let mut events = mio::Events::with_capacity(1024);
+
         while !runtime.borrow().tasks.is_empty() {
             if runtime.borrow().ready_queue.is_empty() {
                 runtime.borrow().poll.poll(&mut events, None)?;
@@ -63,11 +144,11 @@ impl Runtime {
             }
 
             loop {
-                // TODO: why do I get BorroMutError if I use while let? lifetime of temp objects is extended??
+                // Work around https://github.com/rust-lang/rust/issues/38355
                 let res = runtime.borrow_mut().ready_queue.pop_front();
                 match res {
                     Some((mut ready_task, key)) => {
-                        let waker = Waker::new(runtime.clone(), key);
+                        let waker = new_waker(key);
                         let mut ctx = task::Context::from_waker(&waker);
                         match ready_task.as_mut().poll(&mut ctx) {
                             task::Poll::Ready(()) => {
@@ -92,72 +173,24 @@ impl Runtime {
     }
 }
 
-#[derive(Clone)]
-struct Waker {
-    runtime: RuntimePtr,
-    task_idx: usize,
+struct Registration {
+    runtime: RuntimeInnerHandle,
+    key: usize,
 }
-
-impl Waker {
-    fn new(runtime: RuntimePtr, task_idx: usize) -> task::Waker {
-        let waker = Box::new(Waker { runtime, task_idx });
-        unsafe {
-            let raw_waker = task::RawWaker::new(std::mem::transmute(Box::into_raw(waker)), &WAKER_VTABLE);
-            task::Waker::from_raw(raw_waker)
-        }
-    }
-
-    fn wake(&self) {
-        // println!("wake() called for task {}!", self.task_idx);
-        let mut runtime = self.runtime.borrow_mut();
-        let task = runtime.tasks.get_mut(self.task_idx).expect("can't wake nonexistent task");
-        if let Some(task) = task.take() {
-            runtime.ready_queue.push_back((task, self.task_idx))
-        }
-    }
-}
-
-static WAKER_VTABLE: task::RawWakerVTable = {
-    unsafe fn from_ptr(ptr: *const ()) -> Box<Waker> {
-        Box::from_raw(std::mem::transmute(ptr))
-    }
-
-    unsafe fn clone_fn(waker: *const ()) -> task::RawWaker {
-        let waker: *const Waker = std::mem::transmute(waker);
-        let cloned_ptr = std::mem::transmute(Box::into_raw(Box::new((*waker).clone())));
-        task::RawWaker::new(cloned_ptr, &WAKER_VTABLE)
-    }
-
-    unsafe fn wake_fn(waker: *const ()) {
-        // consumes the Box
-        from_ptr(waker).wake();
-    }
-
-    unsafe fn wake_by_ref_fn(waker: *const ()) {
-        let waker: *const Waker = std::mem::transmute(waker);
-        (&*waker).wake();
-    }
-
-    unsafe fn drop_fn(waker: *const ()) {
-        drop(from_ptr(waker));
-    }
-
-    task::RawWakerVTable::new(clone_fn, wake_fn, wake_by_ref_fn, drop_fn)
-};
 
 struct ReadFuture<'a, 'b> {
     stream: &'a mio::net::TcpStream,
-    buf: &'b mut [u8],
+    registration: Option<Registration>,
 
-    runtime: RuntimePtr,
-    registered_as: Option<usize>,
+    buf: &'b mut [u8],
 }
 
 impl ReadFuture<'_, '_> {
-    fn new<'a, 'b>(stream: &'a mio::net::TcpStream, buf: &'b mut [u8], runtime: RuntimePtr) -> ReadFuture<'a, 'b> {
+    fn new<'a, 'b>(stream: &'a mio::net::TcpStream, buf: &'b mut [u8]) -> ReadFuture<'a, 'b> {
         ReadFuture {
-            stream, buf, runtime,
-            registered_as: None,
+            stream,
+            registration: None,
+            buf,
         }
     }
 }
@@ -171,18 +204,20 @@ impl Future for ReadFuture<'_, '_> {
             Ok(nread) => task::Poll::Ready(Ok(nread)),
 
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                let mut runtime = this.runtime.borrow_mut();
-                match this.registered_as {
-                    Some(key) => {
-                        runtime.io_resources.get_mut(key).expect("unknown io resource").push(ctx.waker().clone());
+                match &this.registration {
+                    Some(Registration { runtime, key }) => {
+                        runtime.borrow_mut()
+                            .io_resources.get_mut(*key).expect("unknown io resource")
+                            .push(ctx.waker().clone());
                     }
 
                     None => {
                         // register the waker with poll.
                         // waker is associated with toplevel future
                         // , so we must save the association io_resource -> waker somewhere.
+                        let runtime = current_runtime();
                         let (mut io_resources, poll) = RefMut::map_split(
-                            runtime,
+                            runtime.borrow_mut(),
                             |r| (&mut r.io_resources, &mut r.poll));
                         let entry = io_resources.vacant_entry();
                         let key = entry.key();
@@ -191,7 +226,7 @@ impl Future for ReadFuture<'_, '_> {
                             return task::Poll::Ready(Err(e))
                         }
                         entry.insert(vec![ctx.waker().clone()]);
-                        this.registered_as = Some(key);
+                        this.registration = Some(Registration { runtime: runtime.clone(), key });
                     }
                 }
 
@@ -204,34 +239,26 @@ impl Future for ReadFuture<'_, '_> {
 }
 
 fn main() -> io::Result<()> {
-    let runtime = Runtime::new()?;
-
-    let task = {
-        let runtime = runtime.clone();
-        async move {
-            let res: Result<(), Box<dyn std::error::Error>> = try {
-                let addr = std::env::args().nth(1).unwrap_or("127.0.0.1:8000".to_string());
-                println!("connecting to {}", addr);
-                let stream = mio::net::TcpStream::connect(&addr.parse()?)?;
-                let mut buf = [0u8; 4096];
-                loop {
-                    let nread = ReadFuture::new(&stream, &mut buf, runtime.clone()).await?;
-                    if nread == 0 {
-                        println!("server ended the connection");
-                        break;
-                    } else {
-                        println!("read: {}", String::from_utf8(buf[..nread].to_vec())?);
-                    }
+    let mut runtime = Runtime::new()?;
+    runtime.run(async move {
+        let res: Result<(), Box<dyn std::error::Error>> = try {
+            let addr = std::env::args().nth(1).unwrap_or("127.0.0.1:8000".to_string());
+            println!("connecting to {}", addr);
+            let stream = mio::net::TcpStream::connect(&addr.parse()?)?;
+            let mut buf = [0u8; 4096];
+            loop {
+                let nread = ReadFuture::new(&stream, &mut buf).await?;
+                if nread == 0 {
+                    println!("server ended the connection");
+                    break;
+                } else {
+                    println!("read: {}", String::from_utf8(buf[..nread].to_vec())?);
                 }
-            };
-
-            if let Err(e) = res {
-                eprintln!("error: {}", e);
             }
+        };
+
+        if let Err(e) = res {
+            eprintln!("error: {}", e);
         }
-    };
-
-
-    runtime.borrow_mut().spawn(task);
-    Runtime::run(&runtime)
+    })
 }
